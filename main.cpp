c823f6747a271241014740deecc243ae5d269785
bprado <bastian.prado.v@gmail.com>
2023-12-05 19:07:59 -0800

    Simple commands

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    // variable
+    int file;
+    file = 5;
+    std::cout << file << std::endl;
+
+
+    // const
+    const double pi = 3.14;
+    std::cout << pi << std::endl;
+
+    // naming conventions
+    int file_size; // Snake Case
+    int FileSize; // Pascal Case
+    int fileSize; // Camel Case
+    int iFileSize; // Hungarian Notation
+    
+    // math expressions
+    int x = 5;
+    int y = 4;
+    int multiplication = x*y;
+    int sum = x+y;
+    int subtraction = x-y;
+    int division = x/y;
+
+
+
     return 0;
 }
