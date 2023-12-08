82b2c5e1811f7139c566a1e33a41030d26b33949
bprado <bastian.prado.v@gmail.com>
2023-12-07 23:33:12 -0800

    Add random methods

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -34,6 +36,42 @@ int main() {
     string name;
     cin >> name;
     cout << "Hello: " << name << endl;
+
+    // math funcs --> #include <cmath>
+    const double result = floor(1.2);
+    cout << result << endl;
+
+    const double resultPow = pow(2,3);
+    cout << "pow: " << resultPow << endl;
+
+    // Comments
+    /*
+     * Block comment
+     */
+
+    // data types
+    int a = 5;
+    float b = 3.12f;
+    long c = 90000l;
+    char letter = 'a'; // single ''
+    bool valid = true;
+    auto variable = 6; // auto keyword infers data type
+    auto varString = "hello world";
+
+    // random number --> #include <cstdlib> , #include <ctime>
+    long elapsedSecond = time(0); // elapsewd seconds since Jan 1 1970
+    srand(elapsedSecond);
+    int randomNumber = rand() % 10;
+    cout << randomNumber << endl;
+
+    // random number generator --> dice
+    const int maxValue = 6;
+    const int minValue = 1;
+    int ranDice = (rand() % (maxValue-minValue+1)) + minValue;
+    int ranDice2 = (rand() % (maxValue-minValue+1)) + minValue;
+
+    cout << "first: " << ranDice << endl;
+    cout << "second: " << ranDice2 << endl;
 
     return 0;
 }
